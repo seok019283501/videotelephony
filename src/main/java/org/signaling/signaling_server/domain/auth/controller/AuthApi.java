f406ff286504c50f2ab4bdba473101f15b5df8be
민석 <47300870+seok019283501@users.noreply.github.com>
2025-01-16 01:04:03 +0900

    feat: 비밀번호 변경 구현 (#17)

--- src/main/java/org/signaling/signaling_server/domain/auth/controller/AuthApi.java
+++ src/main/java/org/signaling/signaling_server/domain/auth/controller/AuthApi.java
@@ -3,6 +3,7 @@ package org.signaling.signaling_server.domain.auth.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.signaling.signaling_server.common.api.Api;
+import org.signaling.signaling_server.domain.auth.dto.request.ChangePasswordRequest;
 import org.signaling.signaling_server.domain.auth.dto.response.ReissueAccessTokenResponse;
 import org.signaling.signaling_server.domain.auth.dto.response.SignInResponse;
 import org.springframework.security.core.Authentication;
@@ -10,8 +11,11 @@ import org.springframework.security.core.Authentication;
 @Tag(name = "Auth Api", description = "인증 관련 API 목록입니다.")
 public interface AuthApi {
     @Operation(summary = "로그아웃을 합니다.", description = "담당자: 최민석")
-    Api<SignInResponse> signOut(Authentication authentication, String accessToken);
+    Api<SignInResponse> signOut(String accessToken);
 
     @Operation(summary = "Access Token을 재발급합니다.", description = "담당자: 최민석")
     Api<ReissueAccessTokenResponse> reissueAccessToken(Authentication authentication, String refreshToken);
+
+    @Operation(summary = "비밀번호 변경을 합니다.", description = "담당자: 최민석")
+    Api<?> changePassword(Authentication authentication, ChangePasswordRequest changePasswordRequest);
 }
